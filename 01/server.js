750fec37eb927820831e2eeb5eb51d6accd77c2c
H4RM4IN <mail.ahmad.harmain@gmail.com>
2016-09-08 23:42:55 +0700

    how to using html file to get respon from server

--- 01/server.js
+++ 01/server.js
@@ -1,7 +1,20 @@
 var http=require('http');
+var fs=require('fs');
 
 function HandleServer(Req,Res) {
-    Res.end("<h1>Hello i'm from NodeJS</h1>")
+    Res.writeHead(200,{'Content-Type':'text/html'});
+
+    fs.readFile('./index.html',null,function(err,data){
+        if(err){
+            Res.writeHead(404);
+            Res.write('File Not Found');
+        }
+        else{
+            Res.write(data);
+        }
+
+        Res.end();
+    })
 }
 
 var server=http.createServer(HandleServer);
